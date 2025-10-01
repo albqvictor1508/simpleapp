28d6049b22a74fd9d78a757f1b3a360985e39ab1
victorarruda460 <victorarruda460@gmail.com>
2025-10-01 11:28:32 +0000

    wip(list): creating list item

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/user.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,30 +7,36 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late final List<ListItemData> _listItems = [
+    ListItemData("Minhas Tarefas", "assets/images/avatar.png"),
+    ListItemData("Minhas Tarefas", "assets/images/avatar.png"),
+    ListItemData("Minhas Tarefas", "assets/images/avatar.png"),
+    ListItemData("Minhas Tarefas", "assets/images/avatar.png"),
+    ListItemData("Minhas Tarefas", "assets/images/avatar.png"),
+    ListItemData("Minhas Tarefas", "assets/images/avatar.png"),
+  ];
+
   void _changeSalve() {
     "salve";
   }
--- lib/user.dart
+++ lib/user.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class User {
   final String name;
   final String email;
@@ -11,4 +13,27 @@ class ListItemData {
   final String avatar;
 
   ListItemData(this.name, this.avatar);
+}
+
+class ListItem extends StatelessWidget {
+  final ListItemData data;
+
+  const ListItem({super.key, required this.data});
+
+  @override
+  Widget build(BuildContext ctx) {
+    return Container(
+      decoration: BoxDecoration(
+        border: Border(bottom: BorderSide(width: 1.0, color: Colors.grey)),
+      ),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [Row(children: [])],
+        ),
+      ),
+    );
+  }
 }
